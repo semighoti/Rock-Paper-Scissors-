6bdf224b7e3445c4ee51f40f6053e567eff64593
semighoti <semighoti@sheet2odoo.com>
2024-04-01 13:28:23 -0700

    Please enter the commit message for your changes. Lines starting 	new file:   gameon.html 	new file:   jvs.js 	new file:   script.js

---
+++ jvs.js
@@ -0,0 +1,10 @@
+const button = document.querySelectorAl("button");
+
+const getBotChoice = () => ["rock", "paper", `scissors`][Math.floor(Math.random() * 3)];
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+  })
+
+});
